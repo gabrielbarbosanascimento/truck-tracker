2ebdb782b9f6bfe5b31ae2f5d4e96d484e9fdcd6
Gabriel Barbosa Nascimento <barbosanascimentogabriel@gmail.com>
2022-11-02 18:41:39 -0300

    initial commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,61 @@
-import { MetaHeading, MetaSubtitle } from '../../core/metads/typography';
-import { motion } from 'framer-motion';
+import {
+  MetaHeading,
+  MetaParagraph,
+  MetaSubtitle
+} from "../../core/metads/typography";
+import { motion } from "framer-motion";
+import { MetaButton, MetaCard, MetaInput } from "../../core/metads/components";
+import styled from "styled-components";
+import { useFormik } from "formik";
 
 export default function Home() {
+  const { handleChange, values, submitForm } = useFormik({
+    initialValues: {
+      username: "",
+      password: ""
+    },
+    onSubmit: (values) => {}
+  });
   return (
-    <div className="p-5">
-      <div
-        style={{ height: '100vh' }}
-        className="d-flex flex-column justify-content-center align-items-center"
-      >
-        <motion.div
-          initial={{ x: -100, opacity: 0 }}
-          animate={{ x: 0, opacity: 1 }}
-          transition={{ duration: 2, delay: 0 }}
-          className="d-flex justify-content-center align-items-center"
-        >
-          <MetaHeading>Projeto Meta DS </MetaHeading>
-        </motion.div>
-        <motion.div
-          initial={{ x: -100, opacity: 0 }}
-          animate={{ x: 0, opacity: .6 }}
-          transition={{ duration: 2, delay: 0.3 }}
-          className="d-flex justify-content-center align-items-center"
-        >
-          <MetaSubtitle>Boilerplate</MetaSubtitle>
-        </motion.div>
-      </div>
-    </div>
+    <LoginPageStyles>
+      <MetaCard>
+        <MetaSubtitle>Login</MetaSubtitle>
+        <MetaParagraph>Faça login para usar o sistema</MetaParagraph>
+        <div className=" mt-3 mb-3">
+          <MetaInput
+            name="username"
+            label="Placa"
+            value={values.username}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mb-3">
+          <MetaInput
+            name="password"
+            label="Senha"
+            value={values.password}
+            type="password"
+            onChange={handleChange}
+          />
+        </div>
+        <MetaButton fullwidth className="primary">
+          Entrar
+        </MetaButton>
+      </MetaCard>
+    </LoginPageStyles>
   );
 }
+
+const LoginPageStyles = styled.div`
+  height: 100vh;
+  display: grid;
+  place-items: center;
+  background-image: url(/assets/login-bg.jpg);
+  background-size: cover;
+  background-repeat: no-repeat;
+
+  ${MetaCard} {
+    width: 90%;
+    max-width: 400px;
+  }
+`;
